26477923ebede354b0fc18670dce18b7c81f9f25
Raphael Kolhaupt <a1407523@unet.univie.ac.at>
2017-05-06 15:38:44 +0200

    SearchActivity erstellt

---
+++ app/src/main/java/at/ac/univie/cosy/viego/search/SearchActivity.java
@@ -0,0 +1,15 @@
+package at.ac.univie.cosy.viego.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import at.ac.univie.cosy.viego.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
