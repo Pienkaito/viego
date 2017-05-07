253d3a2c5a7136b6883dbc3421e31e92c79d392a
MagicMourni <mourni@baerenschwanzel.at>
2017-05-07 20:18:23 +0200

    Wrote Adapter for the ListView. made a first draft for the list items.   //button is out of place still Added code that applies the adapter to the ListView.
    To do:
    Code that turns the picture Id into a real picture.
    Code for API call
    und anderes

--- app/src/main/java/at/ac/univie/cosy/viego/search/SearchAdapter.java
+++ app/src/main/java/at/ac/univie/cosy/viego/search/SearchAdapter.java
@@ -1,26 +1,22 @@
 package at.ac.univie.cosy.viego.search;
 
-import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.nio.BufferUnderflowException;
 import java.util.ArrayList;
 
 import at.ac.univie.cosy.viego.R;
 
 /**
  * Created by Mourni on 07.05.2017.
+ * https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
  */
-/*
+
 public class SearchAdapter extends ArrayAdapter<PlaceInfo> {
     public SearchAdapter(Context context, ArrayList<PlaceInfo> users) {
         super(context, 0, users);
@@ -35,6 +31,21 @@ public class SearchAdapter extends ArrayAdapter<PlaceInfo> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_place, parent, false);
         }
         // Find views from the xml
+        Button button = (Button) convertView.findViewById(R.id.add_button);
+        button.setTag(position);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                int position =(Integer) view.getTag();
+                PlaceInfo objectinfo = getItem(position);
+                //  ADD THIS ITEM TO THE LIST OF DESTINATIONS
+                // ADD A CHECK IF IT HAS ALREADY BEEN ADDED TO THE LIST
+                //  IF ADDED HIDE BUTTON OR SMTH!
+
+            }
+
+        });
+
         TextView place_description = (TextView) convertView.findViewById(R.id.text_place_description);
         TextView place_name = (TextView) convertView.findViewById(R.id.text_place_name);
 
@@ -42,36 +53,8 @@ public class SearchAdapter extends ArrayAdapter<PlaceInfo> {
         //>>> Insert more values, image!
         place_name.setText(placeinfo.place_name);
         place_description.setText(placeinfo.formatted_address);
+
         // Return the completed view to render on screen
         return convertView;
     }
-}
-*/
-
-public class SearchResult extends Activity{
-    // to add:  kurzer code damit die placeresults aus dem API call an diese class weitergegeben werden.
-
-    ListView listview;
-    ArrayAdapter<PlaceInfo> adapter;
-
-
-
-  @Override
-    protected void onCreate (Bundle savedInstanceState){
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_search_result);
-      listview = (ListView) findViewById(R.id.search_results);
-
-      adapter = new ArrayAdapter<PlaceInfo>(this, android.R.layout.simple_list_item_1, placeresult);
-      listview.setAdapter(adapter);
-
-      listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-          @Override
-          public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-              Toast.makeText(getBaseContext(),parent.getItemAtPosition(position)+"is selected", Toast.LENGTH_LONG).show();
-          }
-      });
-
-  }
-
 }
