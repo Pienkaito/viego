35bbb9a9f40e94f48fed5c8d929c90973b1acdd0
Mark Anthony Beringuel <a1400229@unet.univie.ac.at>
2017-05-08 16:29:15 +0200

    Cleaning code and adding comments to MainMenuDrawerView

--- app/src/main/java/at/ac/univie/cosy/viego/view/MainMenuDrawerView.java
+++ app/src/main/java/at/ac/univie/cosy/viego/view/MainMenuDrawerView.java
@@ -1,6 +1,9 @@
-package at.ac.univie.cosy.viego;
+package at.ac.univie.cosy.viego.view;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -17,70 +20,61 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainMenuDrawer extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
+public class MainMenuDrawerView extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
+    private static final float minZoomFactor = 15.0f;
+    private static final float maxZoomFactor = 18.0f;
     private GoogleMap mMap;
+    private LatLng curcoord = new LatLng(48.208456, 16.373130);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+
+        //App Bar init
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //Main Menu Layout init
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.mainmenu_drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        //Google Maps init
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //Navigation Drawer
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
+    /*
+    Sets the app bar drawer with the elements from R.menu.search_app_bar
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.search_app_bar, menu);
+        getMenuInflater().inflate(R.menu.app_bar_drawer, menu);
         return true;
     }
 
+    /*
+    This method handles all the actions made in the app bar.
+    The action bar will automatically handle clicks on the Home/Up button,
+    so long as you specify a parent activity in AndroidManifest.xml.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //Enter Search bar things here
         switch (id) {
             case R.id.app_bar_search:
+                //Enter Search bar things here
                 break;
             default:
                 break;
@@ -89,20 +83,26 @@ public class MainMenuDrawer extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    /*
+    This method handles all the actions made in the navigation drawer
+   */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         switch (id) {
-            case R.id.nav_camera:
+            case R.id.nav_001:
+                //Sights
                 break;
-            case R.id.nav_gallery:
+            case R.id.nav_002:
+                //Start Tour
                 break;
-            case R.id.nav_slideshow:
+            case R.id.nav_003:
+                //Settings
                 break;
-            case R.id.nav_manage:
+            case R.id.nav_004:
+                //About
                 break;
             default:
                 break;
@@ -113,15 +113,27 @@ public class MainMenuDrawer extends AppCompatActivity
         return true;
     }
 
+    /*
+    Method, that initiates the Google Map with default coordinates (Center of Vienna)
+    Also limits the zoom factor by the constants minZoomFactor & maxZoomFactor
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng vie = new LatLng(48.208456, 16.373130);
-        mMap.addMarker(new MarkerOptions().position(vie).title("Marker in Vienna"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(vie));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(18.0f));
-        mMap.setMinZoomPreference(15.0f);
-        mMap.setMaxZoomPreference(18.0f);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(curcoord));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(maxZoomFactor));
+        mMap.setMinZoomPreference(minZoomFactor);
+        mMap.setMaxZoomPreference(maxZoomFactor);
         mMap.setBuildingsEnabled(false);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
